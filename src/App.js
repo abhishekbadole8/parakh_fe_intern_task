7ad58029f20306b82b36b84d7804c586971a76b8
Abhishek Badole <abhishekbadole8@gmail.com>
2023-08-04 18:19:35 +0530

    task ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  const productsData = {
+    data: [
+      {
+        name: "Cosmetics",
+        productList: [
+          { id: 0, name: "Hair Oil", price: 122 },
+          { id: 1, name: "Face wash", price: 123 },
+        ],
+      },
+      {
+        name: "Household",
+        productList: [
+          { id: 2, name: "Hair Oil", price: 122 },
+          { id: 3, name: "Face wash", price: 123 },
+        ],
+      },
+    ],
+  };
+
+  // Add Clicked Product To Cart
+  const addToCart = (product) => {
+    setCart((prevCart) => [...prevCart, product]);
+    setCartCount(cart.length + 1);
+    console.log(`Product added :${JSON.stringify(product)}`);
+  };
+
+  // Remove Clicked Product
+  const removeFromCart = (product) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== product.id));
+    setCartCount(cart.length - 1);
+    console.log(`Product Removed :${JSON.stringify(product)}`);
+  };
+
+  useEffect(() => {
+    console.log(`List of products:${JSON.stringify(cart)}`);
+  }, [cart]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <div className="mycart">
+          <h5>{cartCount}</h5>
+        </div>
+
+        <div className="products">
+          {productsData.data.map((category, categoryIndex) => {
+            return (
+              <div className="product" key={categoryIndex}> 
+                <h4>{category.name}</h4>
+                <hr />
+                <div className="same-category">
+                  {category.productList.map((product, id) => {
+                    return (
+                      <div className="card" key={id}>
+                        <div>
+                          <p>Name: {product.name}</p>
+                          <p>Price: {product.price}</p>
+                        </div>
+                        <div className="buttons">
+                          <button onClick={() => addToCart(product)}>
+                            Add to Cart
+                          </button>
+                          <button onClick={() => removeFromCart(product)}>
+                            Remove from Cart
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 }
